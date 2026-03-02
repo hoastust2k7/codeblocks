26106e33a1e58c280d8927058a0c76cebd47f260
hoastust2k7 <5b3g0036@stust.edu.tw>
2026-03-02 11:01:02 +0800

    'backup'

---
+++ test/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void second() {
+    float weight1, height1, bmi1;
+
+    cout << "========計算BMI========\n";
+    cout << "輸入你的身高（單位：公尺）：";
+    cin >> height1;
+
+    cout << "輸入你的體重（單位：公斤）：";
+    cin >> weight1;
+
+    cout << fixed << setprecision(1);
+    bmi1 = (weight1 / (height1*height1));
+
+    cout << "你的BMI是：";
+    if(bmi1 >= 18.5 && bmi1 < 24) {
+        cout << bmi1 << "正常範圍" << endl;
+    } else if(bmi1 >= 24 && bmi1 < 27) {
+        cout << bmi1 << "異常範圍 >> 過重！" << endl;
+    } else if(bmi1 >= 27 && bmi1 < 30) {
+        cout << bmi1 << "異常範圍 >> 輕度肥胖！" << endl;
+    } else if(bmi1 >= 30 && bmi1 < 35) {
+        cout << bmi1 << "異常範圍 >> 中度肥胖！" << endl;
+    } else if(bmi1 >= 35) {
+        cout << bmi1 << "異常範圍 >> 重度肥胖！" << endl;
+    }
+    else {
+        cout << bmi1 << "體重過輕" << endl;
+    }
+}
+
+int main() {
+    second();
+    return 0;
+}
