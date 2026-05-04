b9c81cdb13d38eeb374a069f277c4f159c78f981
hoastust2k7 <5b3g0036@stust.edu.tw>
2026-05-04 11:47:36 +0800

    backup2

--- 0504/main.cpp
+++ 0504/main.cpp
@@ -156,53 +156,40 @@ void homework1() {
 
         for (int j = 0; j < i; j++) {
             if (inputSelect == 1) {
-                std::cout << "================================" << std::endl;
-                std::cout << "順序：" << j + 1 << std::endl;
-                std::cout << "學號：" << isID[j] << std::endl;
-                std::cout << "姓名：" << isName[j] << std::endl;
-                std::cout << "選擇成績：" << isFirstScore[j] << std::endl;
+                // std::cout << "================================" << std::endl;
+                // std::cout << "順序：" << j + 1 << std::endl;
+                // std::cout << "學號：" << isID[j] << std::endl;
+                // std::cout << "姓名：" << isName[j] << std::endl;
+                // std::cout << "選擇成績：" << isFirstScore[j] << std::endl;
                 // std::cout << "實作成績：" << isSecondScore[j] << std::endl;
                 // std::cout << "總成績：" << isTotalScore[j] << std::endl;
-                std::cout << "================================" << std::endl;
+                // std::cout << "================================" << std::endl;
+                std::cout << j + 1 << " " << isName[j] << " " << isFirstScore[j] << std::endl;
             } else if (inputSelect == 2) {
-                std::cout << "================================" << std::endl;
-                std::cout << "順序：" << j + 1 << std::endl;
-                std::cout << "學號：" << isID[j] << std::endl;
-                std::cout << "姓名：" << isName[j] << std::endl;
-                // std::cout << "選擇成績：" << isFirstScore[j] << std::endl;
-                std::cout << "實作成績：" << isSecondScore[j] << std::endl;
-                // std::cout << "總成績：" << isTotalScore[j] << std::endl;
-                std::cout << "================================" << std::endl;
-            } else {
-                std::cout << "================================" << std::endl;
-                std::cout << "順序：" << j + 1 << std::endl;
-                std::cout << "學號：" << isID[j] << std::endl;
-                std::cout << "姓名：" << isName[j] << std::endl;
+                // std::cout << "================================" << std::endl;
+                // std::cout << "順序：" << j + 1 << std::endl;
+                // std::cout << "學號：" << isID[j] << std::endl;
+                // std::cout << "姓名：" << isName[j] << std::endl;
                 // std::cout << "選擇成績：" << isFirstScore[j] << std::endl;
                 // std::cout << "實作成績：" << isSecondScore[j] << std::endl;
-                std::cout << "總成績：" << isTotalScore[j] << std::endl;
-                std::cout << "================================" << std::endl;
+                // std::cout << "總成績：" << isTotalScore[j] << std::endl;
+                // std::cout << "================================" << std::endl;
+                std::cout << j + 1 << " " << isName[j] << " " << isSecondScore[j] << std::endl;
+            } else {
+                // std::cout << "================================" << std::endl;
+                // std::cout << "順序：" << j + 1 << std::endl;
+                // std::cout << "學號：" << isID[j] << std::endl;
+                // std::cout << "姓名：" << isName[j] << std::endl;
+                // std::cout << "選擇成績：" << isFirstScore[j] << std::endl;
+                // std::cout << "實作成績：" << isSecondScore[j] << std::endl;
+                // std::cout << "總成績：" << isTotalScore[j] << std::endl;
+                // std::cout << "================================" << std::endl;
+                std::cout << j + 1 << " " << isName[j] << " " << isTotalScore[j] << std::endl;
             }
         }
         midExam.close();
     } else {
         std::cerr << "File 404\n";
-    }
-}
-
-int sorting() {
-    int i;
-    for (int k = 0; k < i - 1; k++) {
-        for (int j = 0; j < i - k - 1; j++) {
-            if (stof(isSecondScore[j]) > stof(isSecondScore[j + 1])) {
-                std::string tmp;
-                tmp = isID[j]; isID[j] = isID[j + 1]; isID[j + 1] = tmp;
-                tmp = isName[j]; isName[j] = isName[j + 1]; isName[j + 1] = tmp;
-                tmp = isFirstScore[j]; isFirstScore[j] = isFirstScore[j + 1]; isFirstScore[j + 1] = tmp;
-                tmp = isSecondScore[j]; isSecondScore[j] = isSecondScore[j + 1]; isSecondScore[j + 1] = tmp;
-                tmp = isTotalScore[j]; isTotalScore[j] = isTotalScore[j + 1]; isTotalScore[j + 1] = tmp;
-            }
-        }
     }
 }
 
