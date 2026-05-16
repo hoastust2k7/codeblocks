52bde1bfb775dd3e72c829d6029a39d9c63b6379
chxhua2k7 <xianghuachen2007@gmail.com>
2026-05-16 20:36:00 +0800

    formatting somethings

--- 0512/main.cpp
+++ 0512/main.cpp
@@ -4,74 +4,74 @@
 #include <sstream>
 #include <iomanip>
 
-void priArr(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-}
+// void priArr(int arr[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         std::cout << arr[i] << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
-void sorting(int arrNum[], int arrNumLen) {
-    for (int j = 0; j < arrNumLen - 1; j++) {
-        for (int i = 0; i < arrNumLen - j - 1; i++) {
-            if (arrNum[i] > arrNum[i + 1]) {
-                int tmp = arrNum[i];
-                arrNum[i] = arrNum[i + 1];
-                arrNum[i + 1] = tmp;
-            }
-        }
-    }
+// void sorting(int arrNum[], int arrNumLen) {
+//     for (int j = 0; j < arrNumLen - 1; j++) {
+//         for (int i = 0; i < arrNumLen - j - 1; i++) {
+//             if (arrNum[i] > arrNum[i + 1]) {
+//                 int tmp = arrNum[i];
+//                 arrNum[i] = arrNum[i + 1];
+//                 arrNum[i + 1] = tmp;
+//             }
+//         }
+//     }
 
-    std::cout << "陣列排序後：" << std::endl;
-    for (int i = 0; i < arrNumLen; i++) {
-        std::cout << arrNum[i] << " ";
-    }
-    std::cout << std::endl;
-}
+//     std::cout << "陣列排序後：" << std::endl;
+//     for (int i = 0; i < arrNumLen; i++) {
+//         std::cout << arrNum[i] << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
-void calcing(int arrNum[], int arrNumLen) {
-    int totalArr;
-    int smallArrNum = arrNum[(arrNumLen / arrNumLen) - 1];
-    int largeArrNum = arrNum[arrNumLen - 1];
-    float averArrNum;
-    std::cout << "最小值：" << smallArrNum << std::endl;
-    std::cout << "最大值：" << largeArrNum << std::endl;
+// void calcing(int arrNum[], int arrNumLen) {
+//     int totalArr;
+//     int smallArrNum = arrNum[(arrNumLen / arrNumLen) - 1];
+//     int largeArrNum = arrNum[arrNumLen - 1];
+//     float averArrNum;
+//     std::cout << "最小值：" << smallArrNum << std::endl;
+//     std::cout << "最大值：" << largeArrNum << std::endl;
 
-    for (int i = 0; i < arrNumLen; i++) {
-        averArrNum += arrNum[i];
-    }
-    averArrNum = averArrNum / arrNumLen;
-    std::cout << std::fixed << std::setprecision(1) << "平均值：" << averArrNum;
-}
+//     for (int i = 0; i < arrNumLen; i++) {
+//         averArrNum += arrNum[i];
+//     }
+//     averArrNum = averArrNum / arrNumLen;
+//     std::cout << std::fixed << std::setprecision(1) << "平均值：" << averArrNum;
+// }
 
-void dataing() {
-    int dataLen;
-    int *data;
-    std::cin >> dataLen;
-    data = new int[dataLen];
+// void dataing() {
+//     int dataLen;
+//     int *data;
+//     std::cin >> dataLen;
+//     data = new int[dataLen];
 
-    for (int i = 0; i < dataLen; i++) {
-        std::cout << "第" << i + 1 << "整數：";
-        std::cin >> data[i];
-    }
-    priArr(data, dataLen);
-    sorting(data, dataLen);
-    calcing(data, dataLen);
-    delete [] data;
-}
+//     for (int i = 0; i < dataLen; i++) {
+//         std::cout << "第" << i + 1 << "整數：";
+//         std::cin >> data[i];
+//     }
+//     priArr(data, dataLen);
+//     sorting(data, dataLen);
+//     calcing(data, dataLen);
+//     delete [] data;
+// }
 
 void sortingMidExam(std::string isID[], std::string isName[], std::string isFirstScore[], std::string isSecondScore[], std::string isTotalScore[], int arrWasCount) {
     for (int j = 0; j < arrWasCount - 1; j++) {
-	for (int i = 0; i < arrWasCount - j - 1; i++) {
-	    if (stof(isTotalScore[i]) < stof(isTotalScore[i + 1])) {
-	    	std::string temp;
-		temp = isID[i]; isID[i] = isID[i + 1]; isID[i + 1] = temp;
-		temp = isName[i]; isName[i] = isName[i + 1]; isName[i + 1] = temp;
-		temp = isFirstScore[i]; isFirstScore[i] = isFirstScore[i + 1]; isFirstScore[i + 1] = temp;
-		temp = isSecondScore[i]; isSecondScore[i] = isSecondScore[i + 1]; isSecondScore[i + 1] = temp;
-		temp = isTotalScore[i]; isTotalScore[i] = isTotalScore[i + 1]; isTotalScore[i + 1] = temp;
+	    for (int i = 0; i < arrWasCount - j - 1; i++) {
+	        if (stof(isTotalScore[i]) < stof(isTotalScore[i + 1])) {
+	        	std::string temp;
+	    	temp = isID[i]; isID[i] = isID[i + 1]; isID[i + 1] = temp;
+	    	temp = isName[i]; isName[i] = isName[i + 1]; isName[i + 1] = temp;
+	    	temp = isFirstScore[i]; isFirstScore[i] = isFirstScore[i + 1]; isFirstScore[i + 1] = temp;
+	    	temp = isSecondScore[i]; isSecondScore[i] = isSecondScore[i + 1]; isSecondScore[i + 1] = temp;
+	    	temp = isTotalScore[i]; isTotalScore[i] = isTotalScore[i + 1]; isTotalScore[i + 1] = temp;
+	        }
 	    }
-	}
     }
 }
 
@@ -90,7 +90,7 @@ void calcMidExam(std::string isID[], std::string isName[], std::string isFirstSc
     std::cout << "最大值：" << isTotalScoreHighest << std::endl;
 
     for (int i = 0; i < arrWasCount; i++) {
-	isTotalScoreAver += std::stof(isTotalScore[i]);
+	    isTotalScoreAver += std::stof(isTotalScore[i]);
     }
     isTotalScoreAver = isTotalScoreAver / arrWasCount;
     std::cout << std::fixed << std::setprecision(1) << "平均值：" << isTotalScoreAver << std::endl;
@@ -145,7 +145,7 @@ void readingMidExam() {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     // system("PAUSE");
     // dataing();
     readingMidExam();
